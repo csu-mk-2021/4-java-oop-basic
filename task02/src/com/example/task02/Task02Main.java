bc676c9abefc4b9dec54ef277e00a84d7b25f361
MagTemTenebrius <magtemtenebrius@gmail.com>
2021-09-27 19:20:41 +0500

    task02 without TimeSpan

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,62 @@
 package com.example.task02;
 
 public class Task02Main {
+
+    private int second;
+    private int minute;
+    private int hour;
+
+    public Task02Main() {}
+
+    public Task02Main(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+
+        this.minute += this.second / 60;
+        this.second %= this.second % 60;
+
+        this.hour += this.minute / 60;
+        this.minute %= 60;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public void add(TimeSpan time) {
+
+    }
+
+    public void subtract(TimeSpan time) {
+
+    }
+
+    @Override
+    public String toString() {
+        return this.hour + ":" + this.minute + ":" + this.second;
+    }
+
     public static void main(String[] args) {
 
     }
