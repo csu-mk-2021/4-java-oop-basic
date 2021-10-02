72096f9e286f45bc783ebc26be75a26333b6b040
agridnev228 <agridnev228@gmail.com>
2021-10-02 20:53:30 +0500

    task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,11 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        Complex p1=new Complex(10,10);
+        Complex p2=new Complex(10,10);
+        Complex p3=p1.add(p2);
+        Complex p4=p1.mul(p2);
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
     }
 }
