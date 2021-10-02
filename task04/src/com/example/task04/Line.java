da247c03b497adce342318135623d15472d413f3
agridnev228 <agridnev228@gmail.com>
2021-10-02 21:17:20 +0500

    task04

---
+++ task04/src/com/example/task04/Line.java
@@ -0,0 +1,40 @@
+package com.example.task04;
+
+public class Line {
+
+    private Point p1;
+    private Point p2;
+
+    Line(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+
+    public void setP1(Point p1) {
+        this.p1 = p1;
+    }
+
+    public void setP2(Point p2) {
+        this.p2 = p2;
+    }
+
+    public String toString() {
+        return "[" + p1.toString() + ";" + p2.toString() + "]";
+    }
+
+
+    public boolean isCollinearLine(Point p) {
+        double m1 = ((double) (p.x - this.getP1().x) / (this.getP2().x - this.getP1().x));
+        double m2 = ((double) (p.y - this.getP1().y) / (this.getP2().y - this.getP1().y));
+        return m1 == m2;
+
+    }
+}
