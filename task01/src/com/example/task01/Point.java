5dc067666d15e01a2a7bffdc165670ba03cdbfb7
blackrose198 <grushinskayan@gmail.com>
2021-10-03 20:15:37 +0300

    fix task1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -6,6 +6,27 @@ package com.example.task01;
 public class Point {
     int x;
     int y;
+    public Point(){
+
+    }
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    void flip(){
+        int tmp = -this.x;
+        this.x = -this.y;
+        this.y = tmp;
+
+    }
+    double distance(Point point){
+        double ac = Math.abs(this.x - point.x);
+        double cb = Math.abs(this.y - point.y);
+        return Math.hypot(ac, cb);
+    }
+    public String toString(){
+        return "(" + this.x + ", " + this.y + ")";
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
