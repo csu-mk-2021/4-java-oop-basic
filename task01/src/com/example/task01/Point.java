dfb8b1c1d3ebcd0498748848eb88eb4077074344
raion2578 <ivan.baldin@bk.ru>
2021-10-04 17:38:03 +0500

    task 01 lesson 4

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -11,4 +11,29 @@ public class Point {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
+
+    public Point()
+    {
+
+    }
+
+    public Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public void flip()
+    {
+        int tmp=-this.x;
+        this.x=-this.y;
+        this.y=tmp;
+    }
+
+    public double distance(Point point){
+        return Math.sqrt(Math.pow((this.x - point.x), 2) + Math.pow((this.y - point.y), 2));
+    }
+
+    public String toString(){
+        return "(" + this.x + ", " + this.y + ")";
+    }
 }
