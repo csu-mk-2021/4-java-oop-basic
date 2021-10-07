709b28e0d86cf27dfe26791520d2cd423ebadf4f
raion2578 <ivan.baldin@bk.ru>
2021-10-07 10:16:53 +0500

    task 4 lesson 3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        Complex a = new Complex(1, 2);
+        Complex b = new Complex(3, 4);
+        System.out.println(a.complexAdd(b));
+        System.out.println(a.complexMul(b));
     }
 }
