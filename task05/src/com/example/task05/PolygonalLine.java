bd2733fe8dfc4226f8f372001af2188e9af97158
Ttoni4 <ttoni.email@gmail.com>
2021-10-11 01:30:06 +0500

    task

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,46 +1,38 @@
 package com.example.task05;
+import java.util.*;
 
-/**
- * Ломаная линия
- */
 public class PolygonalLine {
+    private Vector <Point> points=new Vector<Point>();
 
-    /**
-     * Устанавливает точки ломаной линии
-     *
-     * @param points массив точек, которыми нужно проинициализировать ломаную линию
-     */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        this.points = new Vector<Point>();
+
+        for(Point p : points)
+        {
+            if(p == null)
+                throw new IllegalArgumentException("point must be not null!");
+
+            addPoint(p);
+        }
     }
 
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param point точка, которую нужно добавить к ломаной
-     */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        if(point == null)
+            throw new IllegalArgumentException("point must be not null!");
+
+        addPoint(point.getX(), point.getY());
     }
 
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
-     */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        points.add(new Point(x, y));
     }
 
-    /**
-     * Возвращает длину ломаной линии
-     *
-     * @return длину ломаной линии
-     */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double length = 0.0;
+        for (int i = 1; i < points.size(); ++i) {
+            length += points.get(i-1).getLength(points.get(i));
+        }
+        return length;
     }
 
 }
