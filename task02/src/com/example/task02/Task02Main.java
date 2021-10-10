77f5f0a9c7a6deb8298c561d7b91990114e93987
Ttoni4 <ttoni.email@gmail.com>
2021-10-10 16:59:35 +0500

    task

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,13 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
+        TimeSpan t1 = new TimeSpan(54, 65, 10);
+        TimeSpan t2 = new TimeSpan(49, 0, 0);
 
+        //System.out.println(t1.toString());
+
+    //    System.out.println((t1.substract(t2)).toString());
+
+        System.out.println(t2.substract(t1).toString());
     }
 }
---
+++ task02/src/com/example/task02/TimeSpan.java
@@ -0,0 +1,43 @@
+package com.example.task02;
+
+public class TimeSpan {
+    private long seconds;
+    private TimeSpan() {; }
+    private long mktime(int h, int m, int s) {
+        long seconds = s;
+        seconds += m * 60;
+        seconds += h * 3600;
+        return seconds;
+    }
+    public long getSeconds() { return seconds; }
+//Допускаем любые значения аргументов
+    public TimeSpan(int h, int m, int s) {
+        seconds = mktime(h, m, s);
+    }
+
+    public TimeSpan add(TimeSpan t2) {
+        if(t2 == null)
+            throw new IllegalArgumentException("t2 must be not NULL!");
+
+        this.seconds += t2.getSeconds();
+        return this;
+    }
+
+    public TimeSpan substract(TimeSpan t2) {
+        if(t2 == null)
+            throw new IllegalArgumentException("t2 must be not NULL!");
+
+        this.seconds -= t2.getSeconds();
+        return this;
+    }
+
+    //out: "hours.minutes.seconds"
+    public String toString() {
+        String res = String.valueOf(seconds / 3600);//hours
+        res = res.concat(".");
+        res = res.concat(String.valueOf(seconds / 60 % 60));//minutes
+        res = res.concat(".");
+        res = res.concat(String.valueOf(seconds % 60));//seconds
+        return res;
+    }
+}
